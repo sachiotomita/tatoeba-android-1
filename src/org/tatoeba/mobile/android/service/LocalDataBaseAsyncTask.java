b058ade7a59a40448fb51ccd08527c0336caa818
dipdowel <leonid.bogouslavski@gmail.com>
2013-08-30 21:22:38 +0300

    * All the files are now loaded and read through, no data is saved yet though. * Improved quality of code. * Refactored.

--- src/org/tatoeba/mobile/android/service/LocalDataBaseAsyncTask.java
+++ src/org/tatoeba/mobile/android/service/LocalDataBaseAsyncTask.java
@@ -2,20 +2,15 @@ package org.tatoeba.mobile.android.service;
 
 import android.content.Context;
 import android.os.AsyncTask;
-import android.os.Parcel;
-import android.os.ParcelFileDescriptor;
 import android.util.Log;
 import android.widget.ProgressBar;
 import android.widget.ScrollView;
 import android.widget.TextView;
 import au.com.bytecode.opencsv.CSVParser;
-import android.content.res.AssetFileDescriptor;
 import org.tatoeba.mobile.android.WelcomeActivity;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 /**
  * Created with IntelliJ IDEA.
@@ -53,13 +48,12 @@ public class LocalDataBaseAsyncTask extends AsyncTask<String, Integer, Long>
     private final String LINKS_CSV = "db_init/links.csv";
     private final String SENTENCES_CSV = "db_init/sentences.csv";
 
+    private String _currentFile = "none";
+    private String _currentlyDisplayedFile = "none";
+
     /** CSV parser */
     private CSVParser _csvParser;
 
-
-//    TODO:
-//    SIZE_INDEX_FILE, USERS_CSV and other csv fileNames should be placed into an array. This array then has to be
-//    processed element by element. There should be a unified method to parse the CSV and
 
     public LocalDataBaseAsyncTask(Context context)
     {
@@ -87,76 +81,99 @@ public class LocalDataBaseAsyncTask extends AsyncTask<String, Integer, Long>
     {
 //-------------------------------------------------------------------------------------------------------------
 
-        ArrayList<String> tempList =  new ArrayList<String>();
-        tempList.add("temp");
-        tempList.add("temp 1");
-        tempList.add("temp 2");
-
-
-        String[] record = tempList.toArray(new String[tempList.size()]);
-
-
+        String[] record = new String[10];
         _csvParser = new CSVParser('\t');
-        try
+        String[] csvFiles = new String[]{USERS_CSV, LINKS_CSV, SENTENCES_CSV, };
+
+        for (int i=0; i<csvFiles.length; i++)
         {
-            record = _csvParser.parseLine("12312\teng\tThis is a text!");
-        } catch (IOException e)
-        {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
+            _currentFile = csvFiles[i];
 
+            try {
+                BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(_context.getAssets().open(csvFiles[i]), "UTF-8"));
 
-        _splashText.setText("Processing: " + SENTENCES_CSV);
+                // do reading, usually loop until end of file reading
+                String mLine = reader.readLine();
+                while (mLine != null) {
+                    //process line
+                    try
+                    {
+                        _processedDataLength += mLine.getBytes().length;
+                        record = _csvParser.parseLine(mLine);
 
-        try {
-            BufferedReader reader = new BufferedReader(
-                    new InputStreamReader(_context.getAssets().open(SENTENCES_CSV), "UTF-8"));
+                        // parse the CSV record according to which file it belongs to.
+                        parseRecord(record, csvFiles[i]);
 
-            // do reading, usually loop until end of file reading
-            String mLine = reader.readLine();
-            while (mLine != null) {
-                //process line
-                try
-                {
-                    _processedDataLength += mLine.getBytes().length;
-                    record = _csvParser.parseLine(mLine);
-                    _tempCurrentLine = "\n" + record[0] + "|" +record[1] + "|" + record[2];
-                    publishProgress((int) ((_processedDataLength / (float)_totalDataLength ) * 100));
-                }
-                catch (IOException e)
-                {
-                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                        publishProgress((int) ((_processedDataLength / (float) _totalDataLength) * 100));
+                    } catch (IOException e)
+                    {
+                        e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                    }
+
+                    mLine = reader.readLine();
                 }
 
-
-                mLine = reader.readLine();
+                reader.close();
+            } catch (IOException e)
+            {
+                Log.e("###", "Could not open file: " + _currentFile);
             }
-
-            reader.close();
-        } catch (IOException e) {
-            //log the exception
         }
-
 //-------------------------------------------------------------------------------------------------------------
-
-
-
         return _totalDataLength;
     }
 
+    /**
+     * Parses a record according to its type and saves the result to a corresponding property
+     * @param record
+     * @param type
+     */
+    private void parseRecord(String[] record, String type)
+    {
+        String trash = "";
+        try
+        {
+            if (type.equals(SENTENCES_CSV))
+            {
+                //5966	nld	Ik moet gaan slapen.	17
+                trash += record[0]; // sentence id
+                trash += record[1]; // sentence language
+                trash += record[2]; // sentence text
+                trash += record[3]; // user id
+            }
+            else if (type.equals(LINKS_CSV))
+            {
+                //1289	5980
+                trash += record[0]; // left sentence id
+                trash += record[1]; // right sentence id
+
+            }
+            else if (type.equals(USERS_CSV))
+            {
+                //7	Arraroak	Arraroak@tatoeba.org
+                trash += record[0]; // user id
+                trash += record[1]; // user name
+                trash += record[2]; // email
+            }
+        } catch (Exception e)
+        {
+            Log.w("###", "This record failed: " + record.toString());
+        }
+
+    }
+
+
     protected void onProgressUpdate(Integer... progress)
     {
-        //setProgressPercent(progress[0]);
-        //if (_splashText == null) throw new Error("No text view was set!");
-
         if (_progressBar == null) throw new Error("No progress bar was set!");
         _progressBar.setProgress(progress[0]);
-        //_splashText.setText(_tempCurrentLine);
 
-        //_textView.append(progress[0].toString() + "% ");
-        //_scrollView.scrollTo(0, _scrollView.getBottom() );
-        //_textView.scrollTo(0, _textView.getBottom());
-
+        if (!_currentlyDisplayedFile.equals(_currentFile))
+        {
+            _currentlyDisplayedFile = _currentFile;
+            _splashText.setText("Processing: " + _currentFile);
+        }
     }
 
     protected void onPreExecute(String data)
@@ -169,12 +186,9 @@ public class LocalDataBaseAsyncTask extends AsyncTask<String, Integer, Long>
         _welcomeActivity.onDataBaseCreated();
     }
 
-    //public void setVisualAssets(TextView textView, ScrollView scrollView)
     public void setVisualAssets(TextView textView, ProgressBar progressBar, WelcomeActivity welcomeActivity)
     {
-
         _splashText = textView;
-        //_scrollView = scrollView;
         _progressBar = progressBar;
         _welcomeActivity = welcomeActivity;
     }
